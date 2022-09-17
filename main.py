c05d159ec0f7a980408de397456943ad09592028
PressTabs <thomastabsbrooks@gmail.com>
2022-09-16 22:46:32 -0700

    added another method of line generation, fixed issues with random generation

--- main.py
+++ main.py
@@ -7,15 +7,19 @@ Node = namedtuple("Node", ["x_pos", "y_pos", "size", "color"])
 Line = namedtuple("Line", ["node1", "node2", "thickness", "color"])
 
 MIN_X, MAX_X, MIN_Y, MAX_Y = 0, 255, 0, 255
-DEFAULT_SIZE = 2
-DEFAULT_COLOR = (0, 0, 0)
+DEFAULT_SIZE = 4
+DEFAULT_COLOR = None
+DEFAULT_THICKNESS = 2
+SEARCH_RADIUS = 48
 
-NEAREST_NEIGHBOR_BIAS = NNB = 2
-MAX_NUMBER_OF_CONNECTIONS = MNC = 2
+NEAREST_NEIGHBOR_BIAS = NNB = 3
+MAX_NUMBER_OF_CONNECTIONS = MNC = 3
 
-N = 20
+N = 50
 nodes = []
 lines = []
+
+plt.figure(figsize=((MAX_X - MIN_X) / 51, (MAX_Y - MIN_Y) / 51))
 
 
 #   Returns a dot tuple
@@ -31,7 +35,7 @@ def gen_dot() -> type(Node):
 
 def line_gen(node1: type(Node), node2: type(Node)) -> type(Line):
     color = [(node1.color[i] + node2.color[i]) / 2 for i in range(3)]
-    thickness = round((node1.size + node2.size) / 2)
+    thickness = DEFAULT_THICKNESS if DEFAULT_THICKNESS is not None else round((node1.size + node2.size) / 2)
 
     line = Line(node1, node2, thickness, color)
     return line
@@ -55,7 +59,7 @@ def gen_lines(node_list) -> list[Line]:
 
         probability_lines = []
         node1 = node_list[i]
-        for k in range(min(i + 1, N), min(i + MNC, N)):
+        for k in range(min(i + 1, N), N):
             node2 = node_list[k]
             probability_lines.append(1 / (math.dist([node1.x_pos, node1.y_pos], [node2.x_pos, node2.y_pos]) ** NNB))
 
@@ -64,6 +68,20 @@ def gen_lines(node_list) -> list[Line]:
 
         for j in range(min(i + 1, N), min(i + MNC, N)):
             linez.append(line_gen(node1, node_list[min(weighted_probability(normalized_probs) + i + 1, N)]))
+
+    return linez
+
+
+def gen_lines2(node_list) -> list[Line]:
+    linez = []
+
+    for i in range(N):
+        node1 = node_list[i]
+        for j in range(min(i + 1, N), N):
+            node2 = node_list[j]
+            dist = math.dist([node1.x_pos, node1.y_pos], [node2.x_pos, node2.y_pos])
+            if dist < SEARCH_RADIUS:
+                linez.append(line_gen(node1, node2))
 
     return linez
 
@@ -83,5 +101,5 @@ def draw(node_list, line_list):
 
 if __name__ == "__main__":
     nodes = [gen_dot() for _ in range(N)]
-    lines = gen_lines(nodes)
+    lines = gen_lines2(nodes)
     draw(nodes, lines)
